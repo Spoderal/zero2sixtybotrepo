a3d90d23b877680dda49269b11826ab5f53186ae
inergy <inergy.gg@gmail.com>
2022-08-08 21:36:45 -0700

    chore: add a common utils module

---
+++ common/utils.js
@@ -0,0 +1,16 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function toCurrency(n) {
+  console.log(n);
+  const num = parseInt(n);
+  if (num < 0) console.warn("toCurrency: `num` is < 0");
+
+  return `$${numberWithCommas(num)}`;
+}
+
+module.exports = {
+  numberWithCommas,
+  toCurrency,
+};
