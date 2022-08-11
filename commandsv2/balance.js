9765ec02e38d031369603f5d80956d0cf40829f9
inergy <inergy.gg@gmail.com>
2022-08-11 13:24:29 -0700

    chore: restore blance command file to normal

--- commandsv2/balance.js
+++ commandsv2/balance.js
@@ -1,13 +1,13 @@
-const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
-// const { SlashCommandBuilder } = require("@discordjs/builders");
-// const { numberWithCommas, toCurrency } = require("../common/utils");
-// const {
-//   userFindOrCreateInDB,
-//   userGetFromInteraction,
-// } = require("../common/user");
-// const { tipFooterRandom } = require("../common/tips");
-// const { emotes } = require("../common/emotes");
-// const colors = require("../common/colors");
+const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { numberWithCommas, toCurrency } = require("../common/utils");
+const {
+  userFindOrCreateInDB,
+  userGetFromInteraction,
+} = require("../common/user");
+const { tipFooterRandom } = require("../common/tips");
+const { emotes } = require("../common/emotes");
+const colors = require("../common/colors");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,83 +21,80 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    console.time("test");
-    // const user = userGetFromInteraction(interaction);
-    // const profile = await userFindOrCreateInDB(user);
-    // const {
-    //   cash,
-    //   gold,
-    //   rp,
-    //   cmaps: barnmaps,
-    //   ucmaps: ubarnmaps,
-    //   rmaps: rbarnmaps,
-    //   lmaps: lbarnmaps,
-    //   ckeys,
-    //   rkeys,
-    //   ekeys,
-    //   dkeys,
-    //   bank,
-    //   banklimit,
-    //   noto: notoriety,
-    //   wheelspins,
-    //   swheelspins,
-    // } = profile;
+    const user = userGetFromInteraction(interaction);
+    const profile = await userFindOrCreateInDB(user);
+    const {
+      cash,
+      gold,
+      rp,
+      cmaps: barnmaps,
+      ucmaps: ubarnmaps,
+      rmaps: rbarnmaps,
+      lmaps: lbarnmaps,
+      ckeys,
+      rkeys,
+      ekeys,
+      dkeys,
+      bank,
+      banklimit,
+      noto: notoriety,
+      wheelspins,
+      swheelspins,
+    } = profile;
 
-    // if (typeof cash === "undefined") {
-    //   await interaction.reply(
-    //     "You haven't started yet! Use `/start` to create a profile and begin!"
-    //   );
-    // } else {
-    //   let embed = new EmbedBuilder()
-    //     .setTitle(`${user.username}'s Balance`)
-    //     .setDescription(
-    //       `
-    //       ${emotes.cash} Z Cash: ${toCurrency(cash)}\n
-    //       ${emotes.bank} Bank: ${toCurrency(bank)}/${toCurrency(banklimit)}\n
-    //       ${emotes.gold} Gold: ${gold}\n
-    //       ${emotes.rp} RP: ${numberWithCommas(rp)}\n
-    //       ${emotes.wheelSpin} Wheel spins: ${wheelspins}\n
-    //       ${emotes.superWheel} Super Wheel spins: ${swheelspins}\n
-    //     `
-    //     )
-    //     .setColor(colors.blue)
-    //     .setThumbnail("https://i.ibb.co/FB8RwK9/Logo-Makr-5-Toeui.png")
-    //     .setFooter(tipFooterRandom)
-    //     .setFields([
-    //       {
-    //         name: "Barn Maps",
-    //         value: `
-    //           ${emotes.barnMapCommon} Common: ${numberWithCommas(barnmaps)}
-    //           ${emotes.barnMapUncommon} Uncommon: ${numberWithCommas(ubarnmaps)}
-    //           ${emotes.barnMapRare} Rare: ${numberWithCommas(rbarnmaps)}
-    //           ${emotes.barnMapLegendary} Legendary: ${numberWithCommas(
-    //           lbarnmaps
-    //         )}
-    //         `,
-    //         inline: true,
-    //       },
-    //       {
-    //         name: "Keys",
-    //         value: `
-    //           ${emotes.commonKey} Common: ${ckeys}
-    //           ${emotes.rareKey} Rare: ${rkeys}
-    //           ${emotes.exoticKey} Exotic: ${ekeys}
-    //           ${emotes.dirftKey} Drift: ${dkeys}
-    //         `,
-    //         inline: true,
-    //       },
-    //       {
-    //         name: "Event Items",
-    //         value: `
-    //           ${emotes.notoriety} Notoriety: ${numberWithCommas(notoriety)}
-    //         `,
-    //         inline: true,
-    //       },
-    //     ]);
-    // console.timeEnd("test completed");
-    // await interaction.reply({ embeds: [embed] });
-    await interaction.reply("test reply");
-    console.timeEnd("test");
-    // }
+    if (typeof cash === "undefined") {
+      await interaction.reply(
+        "You haven't started yet! Use `/start` to create a profile and begin!"
+      );
+    } else {
+      let embed = new EmbedBuilder()
+        .setTitle(`${user.username}'s Balance`)
+        .setDescription(
+          `
+          ${emotes.cash} Z Cash: ${toCurrency(cash)}\n
+          ${emotes.bank} Bank: ${toCurrency(bank)}/${toCurrency(banklimit)}\n
+          ${emotes.gold} Gold: ${gold}\n
+          ${emotes.rp} RP: ${numberWithCommas(rp)}\n
+          ${emotes.wheelSpin} Wheel spins: ${wheelspins}\n
+          ${emotes.superWheel} Super Wheel spins: ${swheelspins}\n
+        `
+        )
+        .setColor(colors.blue)
+        .setThumbnail("https://i.ibb.co/FB8RwK9/Logo-Makr-5-Toeui.png")
+        .setFooter(tipFooterRandom)
+        .setFields([
+          {
+            name: "Barn Maps",
+            value: `
+              ${emotes.barnMapCommon} Common: ${numberWithCommas(barnmaps)}
+              ${emotes.barnMapUncommon} Uncommon: ${numberWithCommas(ubarnmaps)}
+              ${emotes.barnMapRare} Rare: ${numberWithCommas(rbarnmaps)}
+              ${emotes.barnMapLegendary} Legendary: ${numberWithCommas(
+              lbarnmaps
+            )}
+            `,
+            inline: true,
+          },
+          {
+            name: "Keys",
+            value: `
+              ${emotes.commonKey} Common: ${ckeys}
+              ${emotes.rareKey} Rare: ${rkeys}
+              ${emotes.exoticKey} Exotic: ${ekeys}
+              ${emotes.dirftKey} Drift: ${dkeys}
+            `,
+            inline: true,
+          },
+          {
+            name: "Event Items",
+            value: `
+              ${emotes.notoriety} Notoriety: ${numberWithCommas(notoriety)}
+            `,
+            inline: true,
+          },
+        ]);
+
+      await interaction.reply({ embeds: [embed] });
+    }
   },
 };
