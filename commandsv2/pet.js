4305f7f8e1ccacf873ec9ece4d2498b80049f945
Spoder <alexdowdney@gmail.com>
2022-08-10 22:38:04 -0400

    Create pet.js

--- commandsv2/pet.js
+++ commandsv2/pet.js
@@ -6,7 +6,7 @@ const lodash = require("lodash");
 const partdb = require("../data/partsdb.json");
 const itemdb = require("../data/items.json");
 const petdb = require("../data/pets.json");
-const Cooldowns = require("../schema/cooldowns");
+const Cooldowns = require('../schema/cooldowns')
 const User = require("../schema/profile-schema");
 const colors = require("../common/colors");
 
@@ -16,9 +16,7 @@ module.exports = {
     .setDescription("View your mini miata"),
   async execute(interaction) {
     let userdata = await User.findOne({ id: interaction.user.id });
-    let cooldowndata =
-      (await Cooldowns.findOne({ id: interaction.user.id })) ||
-      new Cooldowns({ id: interaction.user.id });
+    let cooldowndata = await Cooldowns.findOne({id: interaction.user.id}) || new Cooldowns({id: interaction.user.id})
     let pet = userdata.pet;
     if (!pet) return interaction.reply(`You don't have a pet!`);
     let condition = pet.condition;
@@ -427,7 +425,7 @@ module.exports = {
           userdata.save();
         });
       } else if (i.customId.includes("race")) {
-        let timetorace = cooldowndata.pet;
+        let timetorace = cooldowndata.pet
         let timeout = 600000;
         if (timetorace !== null && timeout - (Date.now() - timetorace) > 0) {
           let time = ms(timeout - (Date.now() - timetorace));
@@ -448,16 +446,7 @@ module.exports = {
               },
             }
           );
-          await Cooldowns.findOneAndUpdate(
-            {
-              id: interaction.user.id,
-            },
-            {
-              $set: {
-                "pet.racing": Date.now(),
-              },
-            }
-          );
+         cooldowndata.pet = Date.now()
           
          cooldowndata.save()
 
