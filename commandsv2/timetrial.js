3cd6d4fc4b007be0b46775134219c18fdd2dc67d
inergy <67877482+inergy@users.noreply.github.com>
2022-08-15 16:02:40 -0700

    bug: fix /timetrial errors and actually reply (#38)

--- commandsv2/timetrial.js
+++ commandsv2/timetrial.js
@@ -5,6 +5,7 @@ const Cooldowns = require("../schema/cooldowns");
 const User = require("../schema/profile-schema");
 const colors = require("../common/colors");
 const { emotes } = require("../common/emotes");
+const { randomRange } = require("../common/utils");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,6 +17,7 @@ module.exports = {
         .setDescription("The car id to use")
         .setRequired(true)
     ),
+
   async execute(interaction) {
     const cars = require("../data/cardb.json");
     let moneyearnedtxt = 300;
@@ -25,7 +27,7 @@ module.exports = {
     let userdata = await User.findOne({ id: user.id });
     let cooldowndata =
       (await Cooldowns.findOne({ id: user.id })) ||
-      new Cooldowns({ id: interaction.user.id });
+      new Cooldowns({ id: user.id });
 
     let filteredcar = userdata.cars.filter((car) => car.ID == idtoselect);
     let selected = filteredcar[0] || "No ID";
@@ -34,38 +36,38 @@ module.exports = {
         .setTitle("Error!")
         .setColor(colors.discordTheme.red)
         .setDescription(
-          `That car/id isn't selected! Use \`/ids Select [id] [car to select] to select a car to your specified id!\n\n**Example: /ids Select 1 1995 mazda miata**`
+          `That car/id isn't selected! Use \`/ids Select [id] [car to select]\` to select a car to your specified id!\n
+          Example: \`/ids Select 1 1995 mazda miata\``
         );
+
       return await interaction.reply({ embeds: [errembed] });
     }
 
+    const carInLocalDB = cars.Cars[selected.Name.toLowerCase()];
+
     let timeout = 120000;
+
     let racing = cooldowndata.timetrial;
 
     if (racing !== null && timeout - (Date.now() - racing) > 0) {
       let time = ms(timeout - (Date.now() - racing), { compact: true });
-
       return await interaction.reply(
         `Please wait ${time} before doing the timetrial again.`
       );
     }
 
-    if (cars.Cars[selected.Name.toLowerCase()].Junked) {
+    if (carInLocalDB.Junked) {
       return await interaction.reply("This car is too junked to race, sorry!");
     }
 
     let range = selected.Range;
-    if (cars.Cars[selected.Name.toLowerCase()].Electric) {
-      if (range <= 0) {
-        return await interaction.reply(
-          `Your EV is out of range! Run /charge to charge it!`
-        );
-      }
+    if (carInLocalDB.Electric && range <= 0) {
+      return await interaction.reply(
+        `Your EV is out of range! Run /charge to charge it!`
+      );
     }
 
-    if (range) {
-      selected.Range -= 1;
-    }
+    if (range) selected.Range -= 1;
 
     cooldowndata.timetrial = Date.now();
     cooldowndata.save();
@@ -73,34 +75,33 @@ module.exports = {
     let user1carspeed = selected.Speed;
     let user1carzerosixty = selected.Acceleration;
     let handling = selected.Handling;
-
     let newhandling = handling / 20;
-
     let driftscore = selected.Drift;
     let hp = user1carspeed + newhandling;
     hp - driftscore;
 
     let timernum = 0;
-
     let timer = setInterval(() => {
       timernum++;
     }, 1000);
-    let semote = emotes.speed;
-    let hemote = emotes.handling;
-    let zemote = emotes.zero2sixty;
-    let cemote = emotes.cash;
+
     let embed = new discord.EmbedBuilder()
-      .setTitle("Going around the track!")
+      .setTitle("Going around the track...")
       .addFields([
         {
-          name: `Your ${cars.Cars[selected.toLowerCase()].Emote} ${
-            cars.Cars[selected.toLowerCase()].Name
-          }`,
-          value: `${semote} Speed: ${user1carspeed} MPH\n${zemote} 0-60: ${user1carzerosixty}s\n${hemote} Handling:${handling}`,
+          name: `Your ${carInLocalDB.Emote} ${carInLocalDB.Name}`,
+          value: `
+            ${emotes.speed} Speed: ${user1carspeed} MPH
+            ${emotes.zero2sixty} 0-60: ${user1carzerosixty}s
+            ${emotes.handling} Handling:${handling}
+          `,
         },
       ])
       .setColor(colors.blue)
       .setThumbnail("https://i.ibb.co/Wfk7s36/timer1.png");
+
+    interaction.reply({ embeds: [embed] });
+
     let randomnum = randomRange(2, 4);
     let launchperc = Math.round(hp / randomnum);
     if (randomnum == 2) {
@@ -124,7 +125,7 @@ module.exports = {
         clearInterval(x, timer);
         embed.addFields([
           { name: "Results", value: `Finished in ${timernum}s` },
-          { name: "Earnings", value: `${cemote} $${moneyearned}` },
+          { name: "Earnings", value: `${emotes.cash} $${moneyearned}` },
         ]);
         interaction.editReply({ embeds: [embed] });
         userdata.cash += Number(moneyearned);
@@ -133,9 +134,5 @@ module.exports = {
         return;
       }
     }, 1000);
-
-    function randomRange(min, max) {
-      return Math.round(Math.random() * (max - min)) + min;
-    }
   },
 };
