2f59dea597ae610f5e4977b58fe7aeda499ad807
inergy <inergy.gg@gmail.com>
2022-08-14 17:43:25 -0700

    chore: cleanup `leaderstats` a bit and add logs

--- commandsv2/leaderstats.js
+++ commandsv2/leaderstats.js
@@ -14,6 +14,7 @@ module.exports = {
 
     let users = await User.find({});
     let members = [];
+    console.log(`Found ${users?.length} users`);
 
     for (let user of users) {
       try {
@@ -23,44 +24,48 @@ module.exports = {
       }
     }
 
-    if (!members.length) {
-      await interaction.editReply("The leaderboard is currently empty!");
-      return;
+    console.log(`Created ${members?.length} members`);
+
+    if (!members?.length) {
+      return await interaction.editReply("The leaderboard is currently empty!");
     }
-    console.log(members);
 
     let embed = new Discord.EmbedBuilder()
       .setTitle("Cash Leaderboard")
       .setColor(colors.blue);
 
-    members = members.sort(function (b, a) {
-      return a.cash - b.cash;
-    });
+    console.log("filtering, sorting, and slicing members array");
+    members = members
+      .filter(function BigEnough(value) {
+        return value.cash > 0;
+      })
+      .sort(function (b, a) {
+        return a.cash - b.cash;
+      })
+      .slice(0, 10);
 
-    members = members.filter(function BigEnough(value) {
-      return value.cash > 0;
-    });
+    console.log(`Members array length is now at ${members?.length}`);
+    
+    console.log("Checking for user in top 10...");
+    const userPositionInTopTen = members?.findIndex(
+      (m) => m.id == interaction.user.id
+    );
 
-    let pos = 0;
-
-    for (let obj of members) {
-      pos++;
-      if (obj.id == interaction.user.id) {
-        embed.setFooter({
-          text: `Your position is #${pos} on the cash leaderboard`,
-        });
-      }
+    if (userPositionInTopTen >= 0) {
+      embed.setFooter({
+        text: `Your position is #${
+          userPositionInTopTen + 1
+        } on the cash leaderboard!`,
+      });
     }
 
-    members = members.slice(0, 10);
-
+    console.log("Creating the list of users for the descriptoin...");
     let desc = "";
-
     for (let i = 0; i < members.length; i++) {
       let user = interaction.client.users.cache.get(members[i].id);
       if (!user) return;
       let bal = members[i].cash;
-      desc += `${i + 1}. ${user.tag} - ${toCurrency(bal)}\n`;
+      desc += `${i + 1}. ${user.tag} - ${toCurrency(bal)}.00\n`;
     }
 
     embed.setDescription(desc);
