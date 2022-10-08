046d8a6171e0fe31b3deead5b22d61c8a656fb20
Lint Action <lint-action@samuelmeuli.com>
2022-10-08 19:40:33 +0000

    Automatically fix code style issues with Prettier

--- commandsv2/itemshop.js
+++ commandsv2/itemshop.js
@@ -4,22 +4,21 @@ const colors = require("../common/colors");
 const { toCurrency } = require("../common/utils");
 const itemsdb = require("../data/items.json");
 
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("itemshop")
     .setDescription("Check the item shop"),
   async execute(interaction) {
-
     let items = [];
     for (let i in itemsdb.Other) {
       i = itemsdb.Other[i];
       let item = i;
       console.log(item);
 
-      if(!item.Price == 0){
-        items.push(`${item.Emote} ${item.Name} : **${toCurrency(item.Price)}**`);
-
+      if (!item.Price == 0) {
+        items.push(
+          `${item.Emote} ${item.Name} : **${toCurrency(item.Price)}**`
+        );
       }
     }
 
