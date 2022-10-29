2066037ee5ff32246167333c259c7567a5c2cb42
Alex Dowdney <alexdowdney@gmail.com>
2022-10-29 15:53:45 -0400

    work

--- commandsv2/work.js
+++ commandsv2/work.js
@@ -2,20 +2,28 @@ const Discord = require("discord.js");
 const carsdb = require("../data/cardb.json");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const lodash = require("lodash");
+const ms = require("ms");
 
 const User = require("../schema/profile-schema");
 const Cooldowns = require("../schema/cooldowns");
 const colors = require("../common/colors");
 const { ActionRowBuilder, ButtonBuilder } = require("discord.js");
-
+const {
+  toCurrency,
+  blankInlineField,
+  convertMPHtoKPH,
+} = require("../common/utils");
 const { GET_STARTED_MESSAGE } = require("../common/constants");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("job")
     .setDescription("Manage your job, work, etc")
-    .addSubcommand((cmd) =>
-      cmd.setName("work").setDescription("Work at your job")
+    .addSubcommand((cmd) => cmd
+
+    .setName("work")
+    .setDescription("Work at your job")
+
     )
     .addSubcommand((cmd) =>
       cmd
@@ -46,6 +54,9 @@ module.exports = {
         name: `${job}`,
         rank: 1,
         rating: 0,
+        ratings: [],
+        cooldown:0,
+        shifts:0
       };
       userdata.work = jobtoset;
       userdata.save();
@@ -55,8 +66,24 @@ module.exports = {
       if (!userdata.work) return interaction.reply("You don't have a job.");
 
       let jobtowork = userdata.work;
-
+      
+      let timeout = 28800000
+      if (userdata.work.cooldown !== null && timeout - (Date.now() - userdata.work.cooldown) > 0) {
+        let time = ms(timeout - (Date.now() - userdata.work.cooldown));
+        let timeEmbed = new Discord.EmbedBuilder()
+          .setColor(colors.blue)
+          .setDescription(
+            `You've already worked\n\nWork again in ${time}.`
+          );
+        await interaction.reply({ embeds: [timeEmbed], fetchReply: true });
+        return;
+      }
+      
+      
       if (jobtowork.name == "zuber") {
+        userdata.work.cooldown = Date.now()
+        userdata.markModified("work")
+         userdata.save()
         let locations = [
           { name: "Zero City", miles: 50 },
           { name: "Zero Shores", miles: 30 },
@@ -91,6 +118,7 @@ module.exports = {
           .setDescription(
             `${person.name} is asking to be dropped off at ${location.name}, do you want to take this job?`
           )
+          .setThumbnail("https://i.ibb.co/WKPDFM9/work-zuber.png")
           .addFields({ name: "Miles", value: `${location.miles}` })
           .setColor(colors.blue);
         let msg = await interaction.reply({
@@ -108,6 +136,10 @@ module.exports = {
 
         collector.on("collect", async (i) => {
           if (i.customId == "yes") {
+            userdata.work.cooldown = Date.now()
+            userdata.markModified("work")
+            userdata.update()
+            msg.edit({components: []})
             let msg2 = await i.update({
               content: `Please send the car id you wish to use to transport ${person.name}`,
               fetchReply: true,
@@ -119,8 +151,8 @@ module.exports = {
               filter,
               time: 1000 * 30,
             });
-            collector.on("collect", async (msg) => {
-              let idtoselect = msg.content.toLowerCase();
+            collector.on("collect", async (msg3) => {
+              let idtoselect = msg3.content.toLowerCase();
 
               let filteredcar = userdata.cars.filter(
                 (car) => car.ID == idtoselect
@@ -153,10 +185,20 @@ module.exports = {
                 finalz = 6;
               }
 
-              let score = (speed * finalz) / 100;
+              let score = (speed * finalz) / 50;
               console.log(score);
 
               let miles = 0;
+              let speed2 = `${selected.Speed} MPH`;
+
+              if (userdata.settings.ph == "KMH") {
+                speed = `${Math.floor(convertMPHtoKPH(selected.Speed))} KMH`;
+              }
+              embed.setTitle(`Transporting ${person.name} to ${location.name}`)
+              embed.addFields(
+                {name:`${carsdb.Cars[selected.Name.toLowerCase()].Emote} ${selected.Name}`, value: `Speed: ${speed2}\nAcceleration: ${selected.Acceleration}s\nComfort: ${comfort}`}
+              )
+              msg2.edit({embeds: [embed], components: []})
 
               let x = setInterval(async () => {
                 miles += score;
@@ -193,9 +235,20 @@ module.exports = {
                     }
 
                     console.log(`score: ${finalsc}`);
-                    await msg2.edit(
-                      `Success! you got a rating of ${Math.floor(rating)}/10`
-                    );
+                    await msg2.edit(`Success! you got a rating of ${Math.floor(rating)}/10`);
+                    let salary = rating * 100
+
+                    userdata.cash += salary
+                    userdata.work.ratings.push(rating)
+                    userdata.markModified("work")
+                    let avg = mode(userdata.work.ratings)
+                    userdata.work.rating = avg
+                    userdata.markModified("work")
+
+                    userdata.save()
+
+                    
+
                     clearInterval(x);
                   } else {
                     await msg2.edit("Failed!");
@@ -210,3 +263,11 @@ module.exports = {
     }
   },
 };
+
+function mode(arr){
+  return arr.sort((a,b) =>
+        arr.filter(v => v===a).length
+      - arr.filter(v => v===b).length
+  ).pop();
+}
+
