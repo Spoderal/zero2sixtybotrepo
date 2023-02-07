31df9f541803c2d0bb621a7f0207d8f561cc3e2b
Spoder <alexdowdney@gmail.com>
2023-02-06 22:34:51 -0500

    Update settings.js

--- commandsv2/settings.js
+++ commandsv2/settings.js
@@ -35,9 +35,6 @@ module.exports = {
     if (tipsenabled == true) {
       temote = "✅";
     }
-    if (tradesenabled == true) {
-      tremote = "✅";
-    }
 
     let embed = new Discord.EmbedBuilder()
       .setTitle(`Settings for ${user.username}`)
@@ -80,11 +77,6 @@ module.exports = {
       row.components[2].setStyle("Danger");
       row.components[2].setLabel("Disable Tips");
     }
-    if (tradesenabled == true) {
-      row.components[3].setStyle("Danger");
-      row.components[3].setLabel("Disable Trade Requests");
-    }
-
     let msg = await interaction.reply({
       embeds: [embed],
       components: [row],
@@ -123,7 +115,6 @@ module.exports = {
             { name: "Daily Reward Reminder", value: `${demote}` },
             { name: "Top.gg Vote Reminder", value: `${vemote}` },
             { name: "Tips", value: `${temote}` },
-            { name: "Trade Requests", value: `${tremote}` }
           )
           .setColor(colors.blue);
         await i.update({
@@ -155,7 +146,6 @@ module.exports = {
             { name: "Daily Reward Reminder", value: `${demote}` },
             { name: "Top.gg Vote Reminder", value: `${vemote}` },
             { name: "Tips", value: `${temote}` },
-            { name: "Trade Requests", value: `${tremote}` }
           )
           .setColor(colors.blue);
         await i.update({
@@ -187,7 +177,6 @@ module.exports = {
             { name: "Daily Reward Reminder", value: `${demote}` },
             { name: "Top.gg Vote Reminder", value: `${vemote}` },
             { name: "Tips", value: `${temote}` },
-            { name: "Trade Requests", value: `${tremote}` }
           )
           .setColor(colors.blue);
         await i.update({
@@ -195,40 +184,7 @@ module.exports = {
           components: [row],
           fetchReply: true,
         });
-      } else if (i.customId.includes("trades")) {
-        if (tradesenabled == true) {
-          userdata.settings.trades = false;
-          row.components[3].setStyle("Success");
-          row.components[3].setLabel("Enable Trade Requests");
-        } else if (tradesenabled == false) {
-          userdata.settings.trades = true;
-          row.components[3].setStyle("Danger");
-          row.components[3].setLabel("Disable Trade Requests");
-        }
-        userdata.markModified("settings");
-        userdata.save();
-        if (userdata.settings.trades == true) {
-          tremote = "✅";
-        } else {
-          tremote = "❌";
-        }
-        console.log(`${userdata.settings.trades}`);
-
-        embed = new Discord.EmbedBuilder()
-          .setTitle(`Settings for ${user.username}`)
-          .addFields(
-            { name: "Daily Reward Reminder", value: `${demote}` },
-            { name: "Top.gg Vote Reminder", value: `${vemote}` },
-            { name: "Tips", value: `${temote}` },
-            { name: "Trade Requests", value: `${tremote}` }
-          )
-          .setColor(colors.blue);
-        await i.update({
-          embeds: [embed],
-          components: [row],
-          fetchReply: true,
-        });
-      }
+      } 
     });
   },
 };
