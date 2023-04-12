25b1cf125671d56c11c2d7058f032acf00cf0845
Lint Action <lint-action@samuelmeuli.com>
2023-04-12 00:01:59 +0000

    Automatically fix code style issues with Prettier

--- events/itemshop.js
+++ events/itemshop.js
@@ -8,42 +8,41 @@ async function updateItemShop() {
   let timeout = 604800000;
   let itemshopalr = global.itemshop;
   setInterval(async () => {
-  if (itemshopalr.length == 0) {
-    items = [];
-    let itemarr = [];
-    for (let i in itemsdb) {
-      itemarr.push(itemsdb[i]);
-    }
+    if (itemshopalr.length == 0) {
+      items = [];
+      let itemarr = [];
+      for (let i in itemsdb) {
+        itemarr.push(itemsdb[i]);
+      }
 
-    let filtereditems = itemarr.filter((item) => item.Shop == true);
+      let filtereditems = itemarr.filter((item) => item.Shop == true);
 
-    let randitem1 = lodash.sample(filtereditems);
-    let randitem2 = lodash.sample(filtereditems);
-    let randitem3 = lodash.sample(filtereditems);
-    let randitem4 = lodash.sample(filtereditems);
-    let randitem5 = lodash.sample(filtereditems);
+      let randitem1 = lodash.sample(filtereditems);
+      let randitem2 = lodash.sample(filtereditems);
+      let randitem3 = lodash.sample(filtereditems);
+      let randitem4 = lodash.sample(filtereditems);
+      let randitem5 = lodash.sample(filtereditems);
 
-    let item1 = randitem1.Name;
+      let item1 = randitem1.Name;
 
-    let item2 = randitem2.Name;
+      let item2 = randitem2.Name;
 
-    let item3 = randitem3.Name;
+      let item3 = randitem3.Name;
 
-    let item4 = randitem4.Name;
-    let item5 = randitem5.Name;
+      let item4 = randitem4.Name;
+      let item5 = randitem5.Name;
 
-    items.push(item1);
-    items.push(item2);
-    items.push(item3);
-    items.push(item4);
-    items.push(item5);
+      items.push(item1);
+      items.push(item2);
+      items.push(item3);
+      items.push(item4);
+      items.push(item5);
 
-    global.itemshop = items;
-    global.itemshopcooldown = Date.now();
+      global.itemshop = items;
+      global.itemshopcooldown = Date.now();
 
-    global.save();
-  } else {
-  
+      global.save();
+    } else {
       items = [];
       let itemarr = [];
       for (let i in itemsdb) {
@@ -83,7 +82,7 @@ async function updateItemShop() {
       }
     }
   }, 300000);
-  }
+}
 
 module.exports = {
   updateItemShop,
