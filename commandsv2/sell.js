56059910eeb47c4edf54c46c3c5b33c183a0056b
Spoder <alexdowdney@gmail.com>
2023-04-29 02:19:31 -0400

    Update sell.js

--- commandsv2/sell.js
+++ commandsv2/sell.js
@@ -21,6 +21,11 @@ module.exports = {
         .setName("amount")
         .setDescription("The amount to sell")
         .setRequired(true)
+        .addChoices(
+          {name: "1", value: 1},
+          {name: "5", value: 5},
+          {name: "10", value: 10}
+        )
     ),
 
   async execute(interaction) {
