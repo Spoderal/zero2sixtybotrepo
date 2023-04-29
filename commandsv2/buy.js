23c1b2503a9802738c6a96c932d6e62363da1b6e
Spoder <alexdowdney@gmail.com>
2023-04-29 02:19:15 -0400

    Update buy.js

--- commandsv2/buy.js
+++ commandsv2/buy.js
@@ -29,6 +29,11 @@ module.exports = {
         .setName("amount")
         .setDescription("The amount to buy")
         .setRequired(false)
+        .addChoices(
+          {name: "1", value: 1},
+          {name: "5", value: 5},
+          {name: "10", value: 10}
+        )
     )
     .addBooleanOption((option) =>
       option
