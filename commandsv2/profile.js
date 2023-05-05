4c20cc2e27dd7dfe192d1b3126d002c567171b1b
Lint Action <lint-action@samuelmeuli.com>
2023-05-05 05:13:28 +0000

    Automatically fix code style issues with Prettier

--- commandsv2/profile.js
+++ commandsv2/profile.js
@@ -9,7 +9,7 @@ const { GET_STARTED_MESSAGE } = require("../common/constants");
 const achievementsdb = require("../data/achievements.json");
 const pvpranks = require("../data/ranks.json");
 const titledb = require("../data/titles.json");
-const emotes = require("../common/emotes").emotes
+const emotes = require("../common/emotes").emotes;
 
 const { createCanvas, loadImage } = require("canvas");
 
@@ -44,84 +44,84 @@ module.exports = {
       let car2 = cars[car];
       let price = cardb.Cars[car2.Name.toLowerCase()]?.Price;
       if (price) finalprice += Number(price);
-
     }
 
-    let carsort = cars.sort(function(a, b){return b.Speed-a.Speed});
-    let fastcar = carsort[0]
+    let carsort = cars.sort(function (a, b) {
+      return b.Speed - a.Speed;
+    });
+    let fastcar = carsort[0];
 
-    console.log(fastcar)
+    console.log(fastcar);
 
-    let pvprank = userdata.pvprank 
+    let pvprank = userdata.pvprank;
     let pvpname = pvprank.Rank || "Silver";
 
-    if(pvpname == undefined){
-      pvpname = "Silver"
+    if (pvpname == undefined) {
+      pvpname = "Silver";
     }
 
     let pvpindb = pvpranks[pvpname.toLowerCase()];
-    let achievements = userdata.achievements
-    let userjob = userdata.work || {name: 'No Job', position: 'No Position'}
-    let achivarr = []
-    for(let ach in achievements){
-      let achiev = achievements[ach]
-      let achindb = achievementsdb.Achievements[achiev.name.toLowerCase()]
-      achivarr.push(`${achindb.Emote}`)
+    let achievements = userdata.achievements;
+    let userjob = userdata.work || { name: "No Job", position: "No Position" };
+    let achivarr = [];
+    for (let ach in achievements) {
+      let achiev = achievements[ach];
+      let achindb = achievementsdb.Achievements[achiev.name.toLowerCase()];
+      achivarr.push(`${achindb.Emote}`);
     }
     let cash = userdata.cash;
     finalprice += cash;
 
     let acthelmet = profilepics.Pfps[helmet.toLowerCase()].Image;
-    let showcase = userdata.showcase 
+    let showcase = userdata.showcase;
 
     let embed = new Discord.EmbedBuilder()
-    .setTitle(title)
-    .setAuthor({name: user.username, iconURL: acthelmet})
-    .setDescription(
-      `
+      .setTitle(title)
+      .setAuthor({ name: user.username, iconURL: acthelmet })
+      .setDescription(
+        `
       Race Rank: ${racerank}\n
       Drift Rank: ${driftrank}\n
       Prestige: ${prestige}\n
       PVP Rank: ${pvpindb.emote} ${pvpname} ${pvprank.Wins}\n
       **Tier**: ${tier}
       `
-    )
-    .addFields(
-      {
-        name: "Achievements",
-        value: `${achivarr.join(" ")}`,
-        inline: true
-      },
-      {
-        name: "Job",
-        value: `
+      )
+      .addFields(
+        {
+          name: "Achievements",
+          value: `${achivarr.join(" ")}`,
+          inline: true,
+        },
+        {
+          name: "Job",
+          value: `
         __${userjob.name}__
         ${userjob.position}
         `,
-        inline: true
-      },
-      {
-        name: "Best Car",
-        value:`
+          inline: true,
+        },
+        {
+          name: "Best Car",
+          value: `
         **${fastcar.Emote} ${fastcar.Name}**
         ${emotes.speed} ${fastcar.Speed}
         ${emotes.zero2sixty} ${fastcar.Acceleration}s
         ${emotes.handling} ${fastcar.Handling}
         ${emotes.weight} ${fastcar.WeightStat}
         `,
-        inline: true
-      },
-      {
-        name: "Networth",
-        value: `
+          inline: true,
+        },
+        {
+          name: "Networth",
+          value: `
        ${toCurrency(finalprice)}
-        `
-      },
-      
-    )
-    .setColor(`${colors.blue}`)
-    .setThumbnail(showcase)
-    
-    await interaction.reply({embeds: [embed]})
+        `,
+        }
+      )
+      .setColor(`${colors.blue}`)
+      .setThumbnail(showcase);
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
