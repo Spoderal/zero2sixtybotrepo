13e00ffa264e172dc67e0150d1df77b542df7c08
Spoder <alexdowdney@gmail.com>
2023-05-15 17:00:04 -0400

    Update wheelspin.js

--- commandsv2/wheelspin.js
+++ commandsv2/wheelspin.js
@@ -143,6 +143,8 @@ module.exports = {
           }
           collector.on("collect", async (i) => {
             if (i.customId.includes("keep")) {
+              
+              collector.stop()
               if (usercars.length >= garagespaces) {
                 interaction.channel.send("You garage is full!");
                 return;
@@ -186,6 +188,8 @@ module.exports = {
               await i.update({ embeds: [embed] });
               return;
             } else if (i.customId.includes("sell")) {
+              
+              collector.stop()
               userdata.cash += sellprice;
               userdata.save();
               embed.setTitle("✅");
