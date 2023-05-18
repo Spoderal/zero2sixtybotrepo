7cbfbbc160fbf00f604003635f7bbace01dc5fac
Alex Dowdney <alexdowdney@gmail.com>
2023-05-18 02:08:42 -0400

    cc

--- commandsv2/race.js
+++ commandsv2/race.js
@@ -103,6 +103,8 @@ module.exports = {
     let usercars = userdata.cars;
     let idtoselect = interaction.options.getString("car");
 
+    console.log(idtoselect)
+
     let filteredcar = userdata.cars.filter(
       (car) => car.ID.toLowerCase() == idtoselect.toLowerCase()
     );
