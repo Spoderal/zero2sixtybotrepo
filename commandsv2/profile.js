64b681b59350dec735b13f2eba07cc04ecdf5951
Alex Dowdney <alexdowdney@gmail.com>
2023-07-12 00:52:18 -0400

    eee

--- commandsv2/profile.js
+++ commandsv2/profile.js
@@ -111,6 +111,12 @@ module.exports = {
         let achindb = achievementsdb.Achievements[achiev.name.toLowerCase()];
         achivarr.push(`${achindb.Emote}`);
       }
+      console.log(achivarr)
+      if(achivarr.length == 0) {
+        achivarr = ["No achievements"]
+      }
+      console.log(achivarr)
+
       let cash = userdata.cash;
       finalprice += cash;
 
