6e2a67c0fa6e04c52605bd0c0abd7677791b2160
Alex Dowdney <alexdowdney@gmail.com>
2023-07-12 00:41:53 -0400

    eee

--- commandsv2/restore.js
+++ commandsv2/restore.js
@@ -65,6 +65,8 @@ module.exports = {
       Emote: carindb.Emote,
       Livery: carindb.Image,
       Miles: 0,
+      Gas: 10,
+      MaxGas: 10
     };
 
     for (var i = 0; i < 1; i++) usercars.splice(usercars.indexOf(selected), 1);
