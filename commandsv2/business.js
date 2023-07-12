7ca3ac550fa69b2d86507c04d205effe14609e03
Lint Action <lint-action@samuelmeuli.com>
2023-07-12 02:45:38 +0000

    Automatically fix code style issues with Prettier

--- commandsv2/business.js
+++ commandsv2/business.js
@@ -33,9 +33,8 @@ module.exports = {
       ubusiness.Cooldown !== null &&
       timeout - (Date.now() - ubusiness.Cooldown) > 0
     ) {
-      return
-    }
-    else {
+      return;
+    } else {
       userdata.business.CustomerD = userdata.business.Customers;
       userdata.business.Cooldown = 0;
       userdata.markModified("business");
