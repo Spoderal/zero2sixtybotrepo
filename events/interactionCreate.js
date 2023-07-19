893583f48942e3dbf3a8141767e6019e7ed9d95b
Spoder <alexdowdney@gmail.com>
2023-07-18 23:57:33 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,124 +1,138 @@
-const { createBugCard } = require("../services/trello");
-const { updatePetOnCommands } = require("./pets/updatePetOnCommands");
-const { updateCrew } = require("./crews/updateCrew");
-const Cooldowns = require("../schema/cooldowns");
-const Globals = require("../schema/global-schema");
-const User = require("../schema/profile-schema");
-
-const {
-  blacklistInteractionCheck,
-  userGetFromInteraction,
-} = require("../common/user");
-const { InteractionType } = require("discord.js");
-
-//test
-//test
-module.exports = {
-  name: "interactionCreate",
-  once: false,
-  async execute(interaction) {
-    let command;
-    let options = interaction.options;
-    let user = interaction.user;
-    let guild = interaction.guild;
-    let client = interaction.client;
-    let timeout2 = 5000;
-    try {
-      if (interaction.isSelectMenu()) {
-        await interaction.deferUpdate();
+const Global = require(`../schema/global-schema`);
+const itemsdb = require("../data/items.json");
+const lodash = require("lodash");
+const { randomNoRepeats, randomRange } = require("../common/utils");
+async function updateItemShop() {
+  let global;
+  try {
+    global = await Global.findOne();
+  } catch (err) {
+    console.log(err);
+  }
+  let itemcooldown = global.itemshopcooldown;
+  let items = [];
+  let timeout = 86400000;
+  let itemshopalr = global.itemshop;
+  setInterval(async () => {
+    if (itemshopalr.length == 0) {
+      items = [];
+      let itemarr = [];
+      for (let i in itemsdb) {
+        itemarr.push(itemsdb[i]);
       }
-      if (interaction.isChatInputCommand()) {
-        command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) return;
+      let filtereditems = itemarr.filter((item) => item.Shop == true);
+      var chooser = randomNoRepeats(filtereditems);
 
-        // Command
-        const commandExecutionTimeName = `Command ${interaction.commandName} execution time`;
+      let randitem1 = chooser();
+      let randitem2 = chooser();
+      let randitem3 = chooser();
+      let randitem4 = chooser();
+      let randitem5 = chooser();
+      let randitem6 = chooser();
 
-        let cooldowndata =
-          (await Cooldowns.findOne({ id: interaction.user.id })) ||
-          new Cooldowns({ id: interaction.user.id });
-        let userdata = await User.findOne({ id: interaction.user.id });
+      let item1 = randitem1.Name;
+
+      let item2 = randitem2.Name;
+
+      let item3 = randitem3.Name;
+
+      let item4 = randitem4.Name;
+      let item5 = randitem5.Name;
+
+      let item6 = randitem6.Name;
+
+      items.push(item1);
+      items.push(item2);
+      items.push(item3);
+      items.push(item4);
+      items.push(item5);
+      items.push(item6);
+
+      global.itemshop = items;
+      global.update();
+      global.itemshopcooldown = Date.now();
+
+      global.update();
+      global.save();
+    } else {
+      items = [];
+      let itemarr = [];
+      for (let i in itemsdb) {
+        itemarr.push(itemsdb[i]);
+      }
+
+      let filtereditems = itemarr.filter((item) => item.Shop == true);
+
+      var chooser2 = randomNoRepeats(filtereditems);
+
+      let randitem1 = chooser2();
+      let randitem2 = chooser2();
+      let randitem3 = chooser2();
+      let randitem4 = chooser2();
+      let randitem5 = chooser2();
+      let randitem6 = chooser2();
+
+      let item1 = randitem1.Name;
+
+      let item2 = randitem2.Name;
+
+      let item3 = randitem3.Name;
+
+      let item4 = randitem4.Name;
+      let item5 = randitem5.Name;
+      let item6 = randitem6.Name;
+
+      if (itemcooldown !== null && timeout - (Date.now() - itemcooldown) > 0) {
+        return;
+      } else {
+        console.log(timeout - (Date.now() - itemcooldown));
+        console.log("true");
+        items.push(item1);
+        items.push(item2);
+        items.push(item3);
+        items.push(item4);
+        items.push(item5);
+        items.push(item6);
 
         try {
-          let timeout = 3000;
-          let commandran = cooldowndata.command_ran;
-          if (commandran !== null && timeout - (Date.now() - commandran) > 0) {
-            await interaction.reply({
-              content: "Wait 3 seconds before running another command!",
-              fetchReply: true,
-              ephemeral: true,
-            });
-          } else if (
-            cooldowndata.is_racing !== null &&
-            timeout2 - (Date.now() - cooldowndata.is_racing) > 0
-          ) {
-            cooldowndata.command_ran = Date.now();
-            return await interaction.reply({
-              content: `Wait for your race to finish to run other commands`,
-              fetchReply: true,
-              ephemeral: true,
-            });
-          } else {
-            await updateCrew(interaction);
-            if (userdata) {
-              if (Number.isInteger(userdata.cash) == false) {
-                let bal1 = userdata.cash;
-                console.log(bal1);
-                let bal = Math.trunc(bal1);
-                console.log("bal fixed");
-                console.log(bal);
-                userdata.cash = bal;
-                userdata.save();
-              }
-            }
-            await command.execute(interaction);
+          global.itemshop = items;
+          global.update();
+          global.itemshopcooldown = Date.now();
+
+          global.update();
+          let randomgas = lodash.sample([`up`, `down`]);
+          let randomupdown = [
+            0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1, 1,
+          ];
+          let randominc = lodash.sample(randomupdown);
+          if (randomgas == `up`) {
+            global.gas += randominc;
+            global.update();
+
+          } else if (randomgas == `down`) {
+            global.gas -= randominc;
+            global.update();
+
           }
+
+          if(global.gas < 1){
+            global.gas = 6
+            global.update()
+          }
+
+          global.update();
+          global.save();
         } catch (err) {
           console.log(err);
         }
 
-        cooldowndata.save();
-        // Pets
-        const petExecutionTimeName = "Pet update time";
-
-        await updatePetOnCommands(interaction);
-      } else if (
-        interaction.type == InteractionType.ApplicationCommandAutocomplete
-      ) {
-        command = interaction.client.commands.get(interaction.commandName);
-        // try {
-        //   command.autocomplete(interaction, client);
-        // } catch (err) {
-        //   console.log(err);
-        // }
-      }
-    } catch (error) {
-      if (error) {
-        console.log({ interactionCreateExecuteError: error });
-        createBugCard({
-          error,
-          event: "interactionCreate",
-          command,
-          options,
-          user,
-          guild,
-        });
-      }
-      try {
-        await interaction.reply({
-          content:
-            "An error occurred, please contact our support team with the details.",
-          ephemeral: true,
-          fetchReply: true,
-        });
-      } catch (error) {
-        await interaction.channel.send({
-          content:
-            "An error occurred, please contact our support team with the details.",
-          fetchReply: true,
-        });
+        console.log(item1);
       }
     }
-  },
+  }, 60000);
+}
+
+module.exports = {
+  updateItemShop,
 };
